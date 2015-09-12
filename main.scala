839085d16c9af1c122882857c2e13432e73dafff
Vincent Heuschling <vhe@affini-tech.com>
2015-09-12 15:30:25 +0200

    Using Future

--- main.scala
+++ main.scala
@@ -1,28 +1,40 @@
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
+import scala.concurrent.Await
+import scala.concurrent.Future
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
 
 case object StopMsg;
 case class Msg(text : String)
 
 class HelloActor extends Actor {
   def receive = {
-    case Msg(t) => {
-      if (t == "hello") println(s"  $t back to you")
-      else println(s"  does '$t' mean hello ?")
+    case Msg(t)  => {
+      if (t == "hello") sender ! s"  $t back to you"
+      else sender ! s"  does '$t' mean hello ?"
     }
     case StopMsg => context.stop(self)
-    case _       => println("  huh?")
+    case _       => sender ! "  huh?"
   }
 }
 
 object Main extends App {
   val system = ActorSystem("HelloSystem")
-  // default Actor constructor
   val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
-  helloActor ! Msg("hello")
-  helloActor ! Msg("bonjour")
-  helloActor ! Msg("gutentag")
+
+  implicit val timeout = Timeout(5 seconds)
+
+  val future = helloActor ? Msg("hello")
+  val result = Await.result(future, timeout.duration).asInstanceOf[String]
+  println(result)
+
+  //another way
+  val future2: Future[String] = ask(helloActor, Msg("bonjour")).mapTo[String]
+  val result2 = Await.result(future2, 1 second)
+  println(result2)
 
   helloActor ! StopMsg
 
