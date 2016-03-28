b6d4480854200b3e46b456f910c23f5337134351
Vincent Heuschling <vhe@affini-tech.com>
2016-03-28 19:47:53 +0200

    restored step1

--- step1/main.scala
+++ step1/main.scala
@@ -1,45 +1,21 @@
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
-import scala.concurrent.Await
-import scala.concurrent.Future
-import akka.pattern.ask
-import akka.util.Timeout
-import scala.concurrent.duration._
-
-// using Ask & Future
-
-case object StopMsg;
-case class Msg(text : String)
 
 class HelloActor extends Actor {
   def receive = {
-    case Msg(t)  => {
-      if (t == "hello") sender ! s"  $t back to you"
-      else sender ! s"  does '$t' mean hello ?"
-    }    
-    case StopMsg => context.stop(self)
-    case _       => sender ! "  huh?"
+    case "hello" => println("hello back at you")
+    case _       => println("huh?")
   }
 }
 
 object Main extends App {
   val system = ActorSystem("HelloSystem")
+  // default Actor constructor
   val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "bonjour"
 
-  implicit val timeout = Timeout(5 seconds)
-
-  val future = helloActor ? Msg("hello")
-  //val result = Await.result(future, timeout.duration).asInstanceOf[String]
-  val result = Await.result(future, 1 second).asInstanceOf[String]
-  println(result)
-
-  //another way
-  val future2: Future[String] = ask(helloActor, Msg("bonjour")).mapTo[String]
-  val result2 = Await.result(future2, 1 second)
-  println(result2)
-
-  helloActor ! StopMsg
-  Thread.sleep(5L)
+  Thread.sleep(1000L)
   system.shutdown()
 }
