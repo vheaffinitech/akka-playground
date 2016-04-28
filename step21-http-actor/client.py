9cfe77371ecd00fa5ef1ba93986d758cad21e7fb
Vincent Heuschling <vhe@affini-tech.com>
2016-04-28 10:16:12 +0200

    cleaned

--- step21-http-actor/client.py
+++ step21-http-actor/client.py
@@ -9,6 +9,7 @@ t = datetime.datetime.now()
 for i in range(0,100):
     r = requests.get(host + '/req?name=vincent')
     #r = requests.get(host + '/reqresp?name=vincent')
+    
     t2 = datetime.datetime.now()
     d = t2 - t
     t = t2
